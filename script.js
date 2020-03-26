f8fa88c60ede29899ca69d826779e7b59d307f2d
Mark Finazzo <62255633+markfinazzo@users.noreply.github.com>
2020-03-26 10:19:22 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,35 @@
-var vacationType = prompt ("What type of vacation would you like to go on, Musical, Tropical or Adventurous?");
-
-var vacationType = "musical";
-
-var vacationType = "tropical";
-
-var vacationType = "adventurous";
-
-
-var groupSize = 1;
-
-var groupSize = 3;
-
-var groupSize = 6;
-
-
-var vacationType = prompt ("What type of vacation would you like to go on, Musical, Tropical or Adventurous?");
-if (vacationType === "musical, tropical, adventurous?");
+var vacationType = prompt("What type of vacation would you like to go on, Musical, Tropical or Adventurous?");
+console.log(vacationType);
 
 if (vacationType === "musical") {
-  alert ("New Orleans");
+  vacationLocation = "New Orleans";
 } else if (vacationType === "tropical") {
-  alert ("Mexico");
-} else if (color === "adventurous") {
-  alert ("Grand Canyon");
-
-
-
-
-var vacationType = prompt ("What type of vacation would you like to go on, Musical, Tropical or Adventurous?");
-  if (vacationType === "musical, tropical, adventurous"){
-    prompt ("How many people will be in your party?"); 
-  }
-
-var groupSize = prompt ("How many people will be in your party?");
-
-if (vacationType === "musical") {
-  if (groupSize === 1,2 ) {
-    console.log ("Since you’re a group of" + groupSize + "going on a" + vacationType + "vacation, you should fly first class to New Orleans.")
-  }
+  vacationLocation = "Beach vacation in Mexico";
+} else if (vacationType === "adventurous") {
+  vacationLocation === "Whitewater rafting in the Grand Canyon"
+} else {
+  console.log("Please pick one of the options");
 }
 
-if (vacationType === "musical") {
-  if (groupSize === 3-5 ) {
-    console.log ("Since you’re a group of" + groupSize + "going on a" + vacationType + "vacation, you should take a helicopter to New Orleans.")
-  }
+var groupSize = prompt("How many people will be in your party?");
+console.log(groupSize)
+
+if (groupSize === 1 || groupSize === 2) {
+  groupTotal = ("First-class flight");
+} else if (groupSize >= 3 || groupSize <= 5) {
+  groupTotal = ("Take a helicopter");
+} else {
+  groupTotal = ("Charter a flight");
 }
 
-if (vacationType === "musical") {
-  if (groupSize === >= 6) {
-    console.log ("Since you’re a group of" + groupSize + "going on a" + vacationType + "vacation, you should take a charter flight to New Orleans.")
-  }
-}
-
-if (vacationType === "tropical") {
-  if (groupSize === 1,2 ) {
-    console.log ("Since you’re a group of" + groupSize + "going on a" + vacationType + "vacation, you should fly first class to a beach vacation in Mexico.")
-  }
-}
-
-if (vacationType === "tropical") {
-  if (groupSize === 3-5 ) {
-    console.log ("Since you’re a group of" + groupSize + "going on a" + vacationType + "vacation, you should take a helicopter to a beach vacation in Mexico.")
-  }
-}
-
-if (vacationType === "tropical") {
-  if (groupSize === >= 6) {
-    console.log ("Since you’re a group of" + groupSize + "going on a" + vacationType + "vacation, you should take a charter flight to a beach vacation in Mexico.")
-  }
-}
-
-if (vacationType === "adventurous") {
-  if (groupSize === 1,2 ) {
-    console.log ("Since you’re a group of" + groupSize + "going on an" + vacationType + "vacation, you should fly first class to go whitewater rafting in the the Grand Canyon.")
-  }
-}
-
-if (vacationType === "adventurous") {
-  if (groupSize === 3-5 ) {
-    console.log ("Since you’re a group of" + groupSize + "going on an" + vacationType + "vacation, you should take a helicopter to go whitewater rafting in the Grand Canyon.")
-  }
-}
-
-if (vacationType === "adventurous") {
-  if (groupSize === >= 6) {
-    console.log ("Since you’re a group of" + groupSize + "going on an" + vacationType + "vacation, you should take a charter flight to go whitewater rafting in the Grand Canyon.")
-  }
-}
+console.log(
+  "Since you're a group of " +
+    groupSize +
+    " going on a " +
+    vacationType +
+    " vacation, you should " +
+    groupTotal +
+    " to " +
+    vacationLocation +
+    "."
+);
